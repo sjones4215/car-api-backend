046c3c7fec67aa792e67db7e123c4293bf622135
Shawn Jones <bluesfansince2013@gmail.com>
2020-09-22 15:37:39 -0500

    remove info for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_201955) do
+ActiveRecord::Schema.define(version: 2020_09_22_203638) do
 
   create_table "cars", force: :cascade do |t|
     t.integer "year"

6c0248a045799a01eaea5d308b2431df5e96c6ad
Shawn Jones <bluesfansince2013@gmail.com>
2020-09-22 15:32:45 -0500

    heroku changes to description

--- db/migrate/20200922201005_add_description_to_cars.rb
+++ db/migrate/20200922201005_add_description_to_cars.rb
@@ -1,5 +1,5 @@
 class AddDescriptionToCars < ActiveRecord::Migration[6.0]
   def change
-    change_column :cars, :description, :string
+    add_column :cars, :description, :string
   end
 end
--- db/migrate/20200922201955_remove_info_from_cars.rb
+++
@@ -1,5 +0,0 @@
-class RemoveInfoFromCars < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :cars, :info
-  end
-end
